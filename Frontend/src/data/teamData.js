d85212cda46cb5cf1e9dfa37657a91339aa1ca13
MattYudha <dewarahmat12334@gmail.com>
2025-06-16 13:29:15 +0700

    Update Image About Us

--- Frontend/src/data/teamData.js
+++ Frontend/src/data/teamData.js
@@ -1,10 +1,16 @@
-import peopleImage from "../assets/pexelscontoh.jpg";
+import orchidImage from "../assets/Team/orchid.png"; // Ini tetap dipertahankan jika masih dibutuhkan
+import ariaImage from "../assets/Team/Aria.png"; // Impor foto Aria
+import defaImage from "../assets/Team/defa.png"; // Impor foto Defa
+import dimasImage from "../assets/Team/Dimas.png"; // Impor foto Dimas
+import mattImage from "../assets/Team/Matt.png"; // Impor foto Matt
+import stefanyImage from "../assets/Team/Stefany.png"; // Impor foto Stefany
+
 
 export const teamMembers = [
-  { name: "Orchitiadi", role: "Machine Learning", img: peopleImage, large: true },
-  { name: "Stefany", role: "Front-End Developer", img: peopleImage },
-  { name: "Rahmat", role: "Front-End Developer", img: peopleImage, large: true },
-  { name: "Aria", role: "Machine Learning", img: peopleImage },
-  { name: "Defhanaya", role: "Back-End Developer", img: peopleImage},
-  { name: "Dimas", role: "Machine Learning", img: peopleImage }
-];
+  { name: "Orchitiadi", role: "Machine Learning", img: orchidImage, large: true }, // Menggunakan 'Matt.png' untuk Orchitiadi
+  { name: "Stefany", role: "Front-End Developer", img: stefanyImage }, // Menggunakan 'Stefany.png'
+  { name: "Rahmat", role: "Front-End Developer", img: mattImage, large: true }, // Menggunakan 'pexelscontoh.jpg' untuk Rahmat
+  { name: "Aria", role: "Machine Learning", img: ariaImage }, // Menggunakan 'Aria.png'
+  { name: "Defnanaya", role: "Back-End Developer", img: defaImage}, // Menggunakan 'defa.png'
+  { name: "Dimas", role: "Machine Learning", img: dimasImage } // Menggunakan 'Dimas.png'
+]; 
